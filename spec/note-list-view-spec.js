5a1488ffd438784732325a7dc9c98116393ed1f7
Becka <rebdok@hotmail.com>
2018-11-15 13:36:57 +0000

    added max chars for note list view

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -7,7 +7,8 @@ const { describe, it, expect, matchers } = require('../test-suite.js')
 describe('Note-list-view', function() {
   describe("toHTML", function() {
     it('returns a string of HTML', function() {
-        var noteListView = new NoteListView()
+        var noteList = new NoteList()
+        var noteListView = new NoteListView(noteList)
         noteListView.noteList.add('Hello')
         expect(noteListView.toHTML()).toEqual('<ul><li><div>Hello</div></li></ul>')
     })
@@ -15,9 +16,19 @@ describe('Note-list-view', function() {
 
   describe("toHTML", function() {
     it('returns a string of HTML', function() {
-        var noteListView = new NoteListView()
+        var noteList = new NoteList()
+        var noteListView = new NoteListView(noteList)
         noteListView.noteList.add('Hi')
         expect(noteListView.toHTML()).toEqual('<ul><li><div>Hi</div></li></ul>')
+    })
+  })
+
+  describe("toHTML", function() {
+    it('only shows a maximum of 20 characters', function() {
+        var noteList = new NoteList()
+        var noteListView = new NoteListView(noteList)
+        noteListView.noteList.add('Hi, this one is a longer note')
+        expect(noteListView.toHTML()).toEqual('<ul><li><div>Hi, this one is a lo</div></li></ul>')
     })
   })
 })
--- src/note-list-view-model.js
+++ src/note-list-view-model.js
@@ -3,10 +3,15 @@
 (function(exports) {
   NoteListView = function(notelist) {
     this.noteList = notelist
+    MAX_CHARS = 20
   }
 
   NoteListView.prototype.toHTML = function () {
     var string = this.noteList.all()
+    if (string.length > MAX_CHARS) {
+      string = string.split('').splice(0, MAX_CHARS).join('')
+    }
+    // console.log(trimmed_string || string)
     return `<ul><li><div>${string}</div></li></ul>`
   };
 
